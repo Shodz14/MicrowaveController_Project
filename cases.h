b4ac24d06ec016ff783881385ed9c900f7d1c83e
Shodz14 <104007300+Shodz14@users.noreply.github.com>
2022-05-22 02:58:37 +0200

    Add files via upload

---
+++ cases.h
@@ -0,0 +1,1 @@
+void case_bc(char* str,unsigned short time);
---
+++ ports.h
@@ -0,0 +1,14 @@
+void PortA_Init(void);
+
+void PortB_Init(void);
+
+void PortE_Init(void);
+
+void PortF_Init(void);
+
+char SW2(void);
+
+char SW1(void);
+	
+char SW3(void);
+
