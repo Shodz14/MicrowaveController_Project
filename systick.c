082524051baa923f56218514d040b6f5d001ba13
PeterEssam22 <105138651+PeterEssam22@users.noreply.github.com>
2022-05-24 15:13:50 +0200

    Add files via upload

---
+++ systick.c
@@ -0,0 +1,35 @@
+#include "tm4c123gh6pm.h"
+#include "systick.h"
+
+void Systick_Init()
+{
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R  = 0x00FFFFFF;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R = 0X05;
+}
+
+void Systick_Wait(unsigned long delay)
+{
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_RELOAD_R = delay - 1;
+	while(!(NVIC_ST_CTRL_R & 0X10000)){};
+}
+
+void Systick_Wait_1ms(unsigned long delay)
+{
+	unsigned long i;
+	for(i = 0; i < delay; i++)
+	{
+		Systick_Wait(16000);
+	}
+}
+
+void Systick_Wait_1us(unsigned long delay)
+{
+	unsigned long i;
+	for(i = 0; i < delay; i++)
+	{
+		Systick_Wait(16);
+	}
+}
---
+++ systick.h
@@ -0,0 +1,8 @@
+
+void Systick_Init(void);
+
+void Systick_Wait(unsigned long delay);
+
+void Systick_Wait_1us(unsigned long delay);
+
+void Systick_Wait_1ms(unsigned long delay);
